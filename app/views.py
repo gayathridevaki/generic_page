b6e5e3f0bb44b9768d801a530e54969b26e3505d
sai prakash <prakashsaivenkat@gmail.com>
2023-11-23 13:15:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dudu(request):
+    return render(request,'dudu.html')
---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bubu(request):
+    return render(request,'bubu.html')
